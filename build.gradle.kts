7b69b38192eace2d0233e628f0065582827c4ff0
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2026-09-02 15:18:01 +0200

    fix: Fix CVE (#46)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,12 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.1.0"
+  id("org.springframework.boot") version "4.1.1"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "7.3.1.8318"
+  id("org.sonarqube") version "7.4.0.8496"
   id("com.github.ben-manes.versions") version "0.53.0"
-  id("org.openapi.generator") version "7.23.0"
+  id("org.openapi.generator") version "7.25.0"
   id("org.ajoberstar.grgit") version "5.3.2"
   id("com.gorylenko.gradle-git-properties") version "4.0.1"
   id("com.github.jk1.dependency-license-report") version "3.1.4"
@@ -49,27 +49,27 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "3.0.3"
+val springDocOpenApiVersion = "3.1.0"
 val springWolfAsyncApiVersion = "1.21.0"
 val springWolfUiAsyncApiVersion = "1.21.0"
-val openApiToolsVersion = "0.2.10"
-val micrometerVersion = "1.7.0"
-val bouncycastleVersion = "1.84"
+val openApiToolsVersion = "0.2.11"
+val micrometerVersion = "1.7.1"
+val bouncycastleVersion = "1.85.2"
 val httpClientVersion = "5.6.4"
 val httpCoreVersion = "5.4.3"
 val kafkaAppender = "0.2.0-RC2"
-val lz4JavaVersion = "1.11.1"
+val lz4JavaVersion = "1.11.2"
 val commonsLang3Version = "3.20.0"
-val temporalVersion = "1.35.0"
-val protobufJavaVersion = "4.35.1"
-val grpcBomVersion = "1.82.0"
-val guavaVersion = "33.6.0-jre"
-val otelVersion = "1.63.0"
+val temporalVersion = "1.38.0"
+val protobufJavaVersion = "4.36.0"
+val grpcBomVersion = "1.83.1"
+val guavaVersion = "33.7.0-jre"
+val otelVersion = "1.65.0"
 val mapStructVersion = "1.6.3"
 val postgresJdbcVersion = "42.7.13"
 val podamVersion = "8.0.2.RELEASE"
 
-val springCloudDepsVersion = "2025.1.2"
+val springCloudDepsVersion = "2025.1.3"
 
 dependencyManagement {
   imports {
